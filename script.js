973f597c8b0000979a06d9de3c592f2d27bebaef
Miguel Angel Bohorquez <mmige24@gmail.com>
2023-03-01 12:51:57 -0500

    Se crea interacción entre java y las opciones del html

--- script.js
+++ script.js
@@ -7,19 +7,76 @@ const button1 = document.querySelector ('.primary-button');
 const inp1 = document.querySelector('.input-email');
 const inp2 = document.querySelector ('.input-password');
 const resul = document.querySelector('.result');*/
+
+
 const sign= document.querySelector('.sign-in'); 
 const desk= document.querySelector('.desk_menu');
+const mob_icon = document.querySelector ('.mob_bttn');
+const menu_mob = document.querySelector ('.contenedor');
+const carrito = document.querySelector ('.carrito');
+const my_order = document.querySelector ('.caja_mob');
 
 
 
 
-sign.addEventListener('click', toggledesk_menu)
+
+sign.addEventListener('click', toggledesk_menu);
+mob_icon.addEventListener('click', togglecontenedor);
+carrito.addEventListener('click', togglecaja_mob);
+
+
+
+
+
 
 
 function toggledesk_menu(){
-    desk.classList.toggle('inactive');
+    const ismob_menu_close=menu_mob.classList.contains('inactive');
+    const iscarritoclose=my_order.classList.contains('inactive');
+    
+    if(!iscarritoclose){
+        my_order.classList.add('inactive');
+    }
+    if(!ismob_menu_close){
+        menu_mob.classList.add('inactive');
+    }
 
+    desk.classList.toggle('inactive');
 }
+//////////////////////////////////////////////
+function togglecontenedor(){
+
+    const iscarritoclose=my_order.classList.contains('inactive');
+    const isdesk_sign_close=desk.classList.contains('inactive');
+    
+    if(!iscarritoclose){
+        my_order.classList.add('inactive');
+    }
+    if(!isdesk_sign_close){
+        desk.classList.add('inactive');
+    }
+
+    menu_mob.classList.toggle('inactive');
+}
+//////////////////////////////////////////////////////
+function togglecaja_mob(){
+
+    const ismob_menu_close=menu_mob.classList.contains('inactive');
+    const isdesk_sign_close=desk.classList.contains('inactive');
+
+    if(!ismob_menu_close){
+        menu_mob.classList.add('inactive');
+    }
+
+    if(!isdesk_sign_close){
+        desk.classList.add('inactive');
+    }
+    
+    
+    my_order.classList.toggle('inactive');
+}
+    
+
 
 
 
